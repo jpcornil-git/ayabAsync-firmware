01cc90b47b305601d41187ace7206792148b3b36
Jean-Philippe (JP) Cornil <jeanphilippe.cornil@gmail.com>
2025-12-03 14:19:40 +0100

    Add dummy GPIO expanders when none are detected
    This avoid crash when a shield is not present and therefore run
    tests on MCU without shields (GPIO updates are transmitted using
    a debugPrint message iback to the application)

--- lib/api/api.h
+++ lib/api/api.h
@@ -60,6 +60,7 @@ enum class AYAB_API : uint8_t {
   indicateState = 0x84,
 
   debugBase = 0xf0,
+  debugPrint = 0xf1,
   debugRequestPeek = 0xf8,
   debugConfirmPeek = 0xf9,
   debugRequestPoke = 0xfa
